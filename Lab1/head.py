5ce342c0d6b47fd91d4ac50c4e831a6b8cf1fbb3
seanmin21 <seanmin@andrew.cmu.edu>
2026-01-22 20:20:08 -0500

    Update head.py

--- Lab1/head.py
+++ Lab1/head.py
@@ -1,1 +1,19 @@
+import hello_helpers.hello_misc as hm
+import numpy as np
+
+node = hm.HelloNode.quick_create('my_test_node')
+
+
+
+#node.move_to_pose({'joint_head_pan': np.radians(60)}, blocking=True)
+node.move_to_pose({'joint_head_tilt': np.radians(60)}, blocking=True)
+
+
+
+
+
+
+
+
+
 
