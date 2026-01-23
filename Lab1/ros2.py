47b650f7d7419ad5df31b810d507928674c27c14
seanmin21 <seanmin@andrew.cmu.edu>
2026-01-22 19:45:22 -0500

    Update ros2.py

--- Lab1/ros2.py
+++ Lab1/ros2.py
@@ -1,5 +1,9 @@
 import hello_helpers.hello_misc as hm
 
 node = hm.HelloNode.quick_create('my_test_node')
-node.move_to_pose({'joint_lift': 0.5})
+
+node.stow_the_robot()
+node.move_to_pose({'joint_lift': 1.1, 'joint_arm' : 0.5}, blocking=True)
+
+
 
