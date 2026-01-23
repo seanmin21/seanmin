82bf0ef3c6b6c36ae4b7b575a55751dc81eb9b3c
seanmin21 <seanmin@andrew.cmu.edu>
2026-01-22 20:02:35 -0500

    Update ros2.py

--- Lab1/ros2.py
+++ Lab1/ros2.py
@@ -13,7 +13,7 @@ node.move_to_pose({'joint_wrist_roll': np.radians(50)}, blocking=True)
 node.move_to_pose({'joint_gripper_finger_left': np.radians(50)}, blocking=True)
 node.move_to_pose({'joint_gripper_finger_left': np.radians(0)}, blocking=True)
 
-node.move_to_pose({'joint_head_pan': np.radians(60)}, blocking=False)
+node.move_to_pose({'joint_head_pan': np.radians(60)})
 node.move_to_pose({'joint_head_tilt': np.radians(60)}, blocking=True)
 
 node.stow_the_robot()
@@ -21,4 +21,5 @@ node.stow_the_robot()
 node.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
 node.move_to_pose({'rotate_mobile_base':np.radians(180)}, blocking=True)
 node.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
+
 
